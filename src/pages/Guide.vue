1dc3924e89300b6a19e23395759c1929c79504a6
JunhaDex <dk.junha.dev@gmail.com>
2023-02-08 23:26:30 +0900

    add layout

--- src/pages/Guide.vue
+++ src/pages/Guide.vue
@@ -1,5 +1,8 @@
 <template>
-  <div class="main">main</div>
+  hello world!
+  <p>{{ foo }}</p>
+  <Icon name="activity" />
+  <v-btn elevation="2">click me!</v-btn>
 </template>
 <script lang="ts" setup>
 import Icon from '@/components/utils/Icon.vue';
@@ -7,4 +10,8 @@ import Icon from '@/components/utils/Icon.vue';
 const foo = 'foo';
 </script>
 <style lang="scss">
+.icon {
+  display: inline-block;
+  color: #00adb5;
+}
 </style>
